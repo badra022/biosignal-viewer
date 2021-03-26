446f4258c289ddd6d78edae96539ebe00ba5885c
badra022 <ahmedbadra29@gmail.com>
2021-03-26 15:14:37 +0200

    new gui

--- main.py
+++ main.py
@@ -12,16 +12,19 @@
 
 # libraries needed for main python file
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QTextEdit
+from PyQt5.QtCore import QDir
 import sys
-from ui import Ui_BiosignalViewe
+from ui import Ui_MainWindow
+import matplotlib
+
 
 
 # class definition for application window components like the ui
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(ApplicationWindow, self).__init__()
-        self.ui = Ui_BiosignalViewe()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
 # function for launching a QApplication and running the ui and main window
