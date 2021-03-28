bd6238c46807c84ab4c0e0276cebda831359c302
badra022 <ahmedbadra29@gmail.com>
2021-03-28 12:09:38 +0200

    finished

--- classes.py
+++ classes.py
@@ -25,25 +25,26 @@ class graph(object):
         return cls.channels[cls.channelIdx - 1]
 
 class signal(object):
-    timer = QtCore.QTimer() 
+    timer = QtCore.QTimer()
+    penColors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)] 
 
     freeChannelsNum = 3
     
     def __init__(self, x, y):
         self.time = x.copy()
         self.amplitude = y.copy()
+        self.zoomFactor = 1
         self.startTimeIdx = 0
-        self.endTimeIdx = 100
-        self.__class__.timer.setInterval(10) # ms interval
-        # self.__class__.timer.timeout.connect(signal.moveGraph)
-        self.pen = pg.mkPen(color=(255, 0, 0))
+        self.startAmpIdx = -1 * self.zoomFactor
+        self.endTimeIdx = 100 * self.zoomFactor
+        self.endAmpIdx = 1 * self.zoomFactor
+        self.__class__.timer.setInterval(30) # ms interval
         if self.__class__.freeChannelsNum > 0:
             self.channelIdx = len(graph.channels) - self.__class__.freeChannelsNum
             self.__class__.freeChannelsNum = self.__class__.freeChannelsNum - 1
             self.plot()
         else:
             print("no free channel is available, clear channels first!")
-        # print("free channels number: ",self.__class__.freeChannelsNum )
         
     def __del__(self):
         try:
@@ -54,15 +55,40 @@ class signal(object):
         
     def plot(self):
         graph.channels[self.channelIdx].setXRange(self.time[self.startTimeIdx], self.time[self.endTimeIdx])
-        self.channel_reference = graph.channels[self.channelIdx].plot(self.time, self.amplitude, pen=self.pen)
-        
+        graph.channels[self.channelIdx].setYRange(-1 * self.zoomFactor , 1 * self.zoomFactor)
+        self.pen = pg.mkPen(color=self.__class__.penColors[self.channelIdx])
+        graph.channels[self.channelIdx].plot(self.time, self.amplitude, pen=self.pen)
 
     def moveGraph(self):
-        self.startTimeIdx = self.startTimeIdx + 1 % (len(self.time) - 100)
+        self.startTimeIdx = (self.startTimeIdx + 1) % (len(self.time) - 100)
         self.endTimeIdx = self.endTimeIdx + 1 % len(self.time)
         graph.channels[self.channelIdx].setXRange(self.time[self.startTimeIdx], self.time[self.endTimeIdx])
-    
+
+    def zoomIn(self):
+        if self.zoomFactor >= 0.2:
+            self.zoomFactor = self.zoomFactor - 0.1
+            self.adjustGraph()
+    def zoomOut(self):
+        if self.zoomFactor < 2.0:
+            self.zoomFactor = self.zoomFactor + 0.1
+            self.adjustGraph()
+            
+    def adjustGraph(self):
+        self.endTimeIdx = int(self.startTimeIdx + (100 * self.zoomFactor))
+        graph.channels[self.channelIdx].setXRange(self.time[self.startTimeIdx], self.time[self.endTimeIdx])
+        graph.channels[self.channelIdx].setYRange(-1 * self.zoomFactor , 1 * self.zoomFactor)
+        
     def getFigure(self):
-        fig = plt.figure()
-        fig.plot(self.time,self.amplitude)
+        fig = plt.figure(figsize=(10, 5))
+        plt.plot(self.time[self.startTimeIdx:self.endTimeIdx],self.amplitude[self.startTimeIdx:self.endTimeIdx])
+        plt.xlabel('time (sec)')
+        plt.ylabel('amplitude (mv)')
+        return fig
+    
+    def getSpectrogram(self):
+        fs = 1/(self.time[1] - self.time[0])
+        fig = plt.figure(figsize=(10, 5))
+        plt.specgram(self.amplitude, Fs= fs)
+        plt.xlabel('time (sec)')
+        plt.ylabel('frequency (Hz)')
         return fig
--- main.py
+++ main.py
@@ -28,21 +28,33 @@ from matplotlib.backends.backend_pdf import PdfPages
 from classes import signal, graph
 
 
-
 # class definition for application window components like the ui
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(ApplicationWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.channelSelector.setRange(1, 3)   # setting range to the spin box
         self.ui.open.triggered.connect(self.open)
         self.ui.clearButton.clicked.connect(self.clearChannels)
         self.ui.startButton.clicked.connect(self.startChannels)
-        self.ui.stopButton.clicked.connect(self.stopChannels) 
+        self.ui.stopButton.clicked.connect(self.stopChannels)
+        self.ui.zoomIn.clicked.connect(self.zoomIn)
+        self.ui.zoomOut.clicked.connect(self.zoomOut)
         self.ui.save.triggered.connect(self.saveAs)
         signal.timer.timeout.connect(self.moveSignals)    
         self.signals = []
-    
+
+    def zoomIn(self):
+            try:
+                self.signals[(self.ui.channelSelector.value() - 1) % 4].zoomIn()
+            except:
+                pass
+    def zoomOut(self):
+            try:
+                self.signals[(self.ui.channelSelector.value() - 1) % 4].zoomOut()
+            except:
+                pass
     def open(self):
         files_name = QtGui.QFileDialog.getOpenFileName( self, 'Open only txt or CSV or xls', os.getenv('HOME') ,"csv(*.csv);; text(*.txt) ;; xls(*.xls)" )
         path = files_name[0]
@@ -87,6 +99,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         report = PdfPages('report.pdf')
         for signal in self.signals:
             report.savefig(signal.getFigure())
+            report.savefig(signal.getSpectrogram())
         report.close()
 
 # function for launching a QApplication and running the ui and main window
